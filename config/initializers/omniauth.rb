2ecb6c7fc9dc2d012d0bff3ad7cd81e2a4459d43
Timenaser <vad-nes@yandex.ru>
2015-03-29 00:33:07 +0400

    add users show

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,  ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+           
   provider :vkontakte, ENV["VK_APP_ID"], ENV["VK_SECRET"]
 end
