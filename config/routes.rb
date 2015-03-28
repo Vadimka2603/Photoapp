e60878747142c4f9d29b627cf46df4542f61a654
Timenaser <vad-nes@yandex.ru>
2015-03-28 22:45:48 +0400

    sign up via soc networks

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'raiting' => 'static_pages#raiting'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+    resources :sessions, only: [:create, :destroy]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
