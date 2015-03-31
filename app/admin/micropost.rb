480d71908bb2c808c2a1fac5f77cb974b4b1fd24
Timenaser <vad-nes@yandex.ru>
2015-03-31 23:26:40 +0400

    Active Admin

--- app/admin/micropost.rb
+++ app/admin/micropost.rb
@@ -1,7 +1,47 @@
 ActiveAdmin.register Micropost do
+config.per_page = 5
+actions :all, :except => [:destroy]
+permit_params :ban_status
+
+scope :all, :default => true
+  scope :banned do |microposts|
+    microposts.where(:ban_status => true)
+  end
+  scope :legal do |microposts|
+    microposts.where(:ban_status => false)
+  end
+
+ index do
+  column :user
+  column "Фото" do |micropost|
+  link_to image_tag(micropost.picture_url), admin_micropost_path(micropost)
+  end
+  column :content
+  actions
+end
 
 
-  permit_params :content, :ban_status
+show :title => "Фото" do
+  link_to image_tag(micropost.picture_url)
+end
 
 
+sidebar "Данные", :only => :show do
+  attributes_table_for micropost do
+  	row :id
+    row :user
+    row :content
+    row :created_at
+    row :ban_status
+  end
+end
+
+  form do |f|
+  	div :class => "form" do 
+    f.inputs 'Забанить' do
+    f.input :ban_status
+    end
+  f.actions
+  end
+end
 end
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register User do
+	config.per_page = 20
+actions :index
+ index do
+  column :id
+  column "Avatar" do |user|
+  link_to image_tag(user.avatar_url)
+  end
+  column :name
+  column :provider
+  column "Number of photos" do |user|
+  user.microposts.count
+  end
+  end
 
 end
