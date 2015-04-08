94aa6f9862653d8b9edffd4c45ed24e276d82011
Vadimka2603 <vad-nes@yandex.ru>
2015-04-08 17:17:48 +0300

    v3

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,7 +9,8 @@ class Micropost < ActiveRecord::Base
 
   mount_uploader :picture, PictureUploader
 
-  validates :user_id, :picture, presence: true
+  validates :user_id, presence: true
+  #validates :picture, presence: true
   validates :content, length: { maximum: 70 }
   validate  :picture_size
 
