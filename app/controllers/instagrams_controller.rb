0b602e6aa836225a93c9b866180b791af8c83452
Timenaser <vad-nes@yandex.ru>
2015-04-13 23:25:52 +0400

    instafix

--- app/controllers/instagrams_controller.rb
+++ app/controllers/instagrams_controller.rb
@@ -1,6 +1,7 @@
 class InstagramsController < ApplicationController
-  
+
   def show
   	render json: params['hub.challenge']
   end
+end
  # Instagram.create_subscription("user", "https://shrouded-garden-5776.herokuapp.com/instagram/callback")
