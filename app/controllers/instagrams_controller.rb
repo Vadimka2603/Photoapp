cc863a4e89afeed99e61b65faa8e8f608fcd6100
Vadimka2603 <vad-nes@yandex.ru>
2015-04-13 16:43:01 +0300

    Add show

--- app/controllers/instagrams_controller.rb
+++ app/controllers/instagrams_controller.rb
@@ -1,6 +1,6 @@
 class InstagramsController < ApplicationController
 
-def show
+def create
     default_search = Instagram.tag_search('Rockiesbaseball')
     if params[:q].blank?
       @tag = default_search
@@ -13,17 +13,20 @@ def show
 end
 
 def create_subscription(*args)
-        options = args.last.is_a?(Hash) ? args.pop : {}
-        object = args.shift
-        callback_url = args.shift
-        aspect = args.shift
-        options.tap {|o|
-          o[:object] = object unless object.nil?
-          o[:callback_url] = callback_url unless callback_url.nil?
-          o[:aspect] = aspect || o[:aspect] || "media"
-        }
-        response = post("subscriptions", options.merge(:client_secret => "442ae4c80b53406e84fd6d4bb6302665"))
-        response
+  options = args.last.is_a?(Hash) ? args.pop : {}
+  object = args.shift
+  callback_url = 'https://shrouded-garden-5776.herokuapp.com/instagram/callback'
+  aspect = args.shift
+  options.tap {|o|
+    o[:object] = object unless object.nil?
+    o[:callback_url] = callback_url unless callback_url.nil?
+    o[:aspect] = aspect || o[:aspect] || "media"
+  }
+  response = post("subscriptions", options.merge(:client_secret => client_secret))
+    response
   end
 
-  
+  def show
+  	render json: params['hub.challenge']
+  end
+ # Instagram.create_subscription("user", "https://shrouded-garden-5776.herokuapp.com/instagram/callback")
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -4,4 +4,5 @@ require "instagram"
     config.client_id = "405c989adc28474fbffe14103ebaf6a1"
     config.client_secret = "442ae4c80b53406e84fd6d4bb6302665"
     config.access_token = "1538797041.405c989.3190ab7514c14d699d5060a6144db65c"
+    
 end
