408f355aa9d9a847d68f40756a0c4cc2b1f3235f
Timenaser <vad-nes@yandex.ru>
2015-04-16 00:33:55 +0400

    POST

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -1,14 +1,25 @@
 module Api
   class MicropostsController < ApplicationController
-
+  	
+    http_basic_authenticate_with name: "admin", password: "secret"
     def index
       @microposts = Micropost.all
       respond_to do |format|
         format.json 
-        
       end
-      
     end
 
+    def create
+    @micropost = Micropost.create(micropost_params)
+      if @micropost.save
+        render json: @micropost
+      end
+    end
+
+    private
+
+    def micropost_params
+      params.require(:micropost).permit(:user_id, :content, :picture)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   mount_uploader :avatar, AvatarUploader
 
-	def self.from_omniauth(auth)
+  def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider 
       user.uid      = auth.uid
@@ -20,5 +20,7 @@ class User < ActiveRecord::Base
   def liking?(micropost)
     liked_microposts.include?(micropost)
   end
+
+  
 
 end
